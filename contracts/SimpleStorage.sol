5babaaf22a00ecb69c8c5079597dcd1bf6f9630d
kilian mongey <tumbleur.kilian@yahoo.fr>
2021-07-15 22:29:38 +0200

    test improvement convert eth => dai

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
-import "@uniswap/v3-periphery/contracts/interfaces/IPeripheryPayments.sol";
-import "@uniswap/v3-periphery/contracts/base/PeripheryPayments.sol";
+import "/Users/kilianmongey/Desktop/HomePageDigiBox/DigiBox/node_modules/@uniswap/v3-periphery/contracts/interfaces/IPeripheryPayments.sol";
+///import "/Users/kilianmongey/Desktop/HomePageDigiBox/DigiBox/node_modules/@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+///import "/Users/kilianmongey/Desktop/HomePageDigiBox/DigiBox/node_modules/@uniswap/v3-periphery/contracts/interfaces/IQuoter.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
@@ -32,6 +32,10 @@ contract SimpleStorage {
     ) internal;
  } */
 
+ ///interface IUniswapRouter is ISwapRouter {
+ ///    function refundETH() external payable;
+ ///}
+
 contract Vault is Ownable {
     
     using SafeMath for uint;
@@ -42,6 +46,11 @@ contract Vault is Ownable {
     uint256 public storedUsdt;
     uint256 public amountMinimum;
     address public token;
+
+  ///IUniswapRouter public constant uniswapRouter = IUniswapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
+  ///IQuoter public constant quoter = IQuoter(0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6);
+  ///address private constant multiDaiKovan = 0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa;
+  ///address private constant WETH9 = 0xd0A1E359811322d97991E03f863a0C30C2cF029C;
     
     event EthReceived(address From, uint256 Amount);
     event UsdtReceived(address From, uint256 Amount);
@@ -69,7 +78,7 @@ contract Vault is Ownable {
         //Iuniswap.pay();
         
         emit ConvertedEthToUsdt(msg.value);
-    }
+    } 
     
     ///@dev helper to keep track of balances
     function getBalanceOfETH() public view returns(uint256) {
