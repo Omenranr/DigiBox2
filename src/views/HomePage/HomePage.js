6b1c4145e9e10fe9c965d6737868941dd51d25b1
kilian mongey <tumbleur.kilian@yahoo.fr>
2021-07-15 17:49:33 +0200

    logo added

--- src/views/HomePage/HomePage.js
+++ src/views/HomePage/HomePage.js
@@ -3,6 +3,8 @@ import NavBar from "../../Components/NavBar/NavBar";
 import NavBarDetail from "../../Components/NavBarDetail/NavBarDetail";
 import NFT from "../../Components/NFT/NFT";
 import Connection from "../../Connection";
+import "./HomePage.css";
+import logo from "../../Images/LogoDigiBox.png";
 
 export default function HomePage() {
     return (
@@ -10,7 +12,10 @@ export default function HomePage() {
         <NavBar />
         <h1 className="title">DigiBox, le plaisir d'offrir!</h1> <hr></hr>
         <Connection />
-        <h2 className="subTitle">Les DigiBox du moment</h2>
+                <div className="text-logo">
+                    <h2 className="subTitle">Les DigiBox du moment</h2>
+                    <img className="logo" src={logo} alt="logo" />
+                </div>
             <NFT />
         <NavBarDetail />
         </div>
