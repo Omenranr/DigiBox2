3fd0a9b2b3a09e6fd52d1c0036e306cbcaa3a546
kilian mongey <tumbleur.kilian@yahoo.fr>
2021-07-15 15:00:01 +0200

    add SC

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,14 +1,76 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
-contract SimpleStorage {
-  uint storedData;
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/IPeripheryPayments.sol";
+import "@uniswap/v3-periphery/contracts/base/PeripheryPayments.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-  function set(uint x) public {
-    storedData = x;
-  }
+/// Voir si on peux importer directement le smart contract PeripheryPayments directement et faire appel "./Periphery"
 
-  function get() public view returns (uint) {
-    return storedData;
-  }
+ /* contract Test {
+ function refundETH() external payable;
+    
+    function pay(
+        address token,
+        address payer,
+        address recipient,
+        uint256 value
+    ) internal;
+ } */
+
+contract Vault is Ownable {
+    
+    using SafeMath for uint;
+    
+    IPeripheryPayments Iuniswap;
+    
+    uint256 public storedEth;
+    uint256 public storedUsdt;
+    uint256 public amountMinimum;
+    address public token;
+    
+    event EthReceived(address From, uint256 Amount);
+    event UsdtReceived(address From, uint256 Amount);
+    event UsdtSentTo(address recipient, uint256 Amount);
+    event ChangedMinAmount(uint256 MinAmountToConvert);
+    event ConvertedEthToUsdt(uint256 Amount);
+    event ChangedAddressToken(address TokenAddress);
+    
+    ///@dev toManage if ethPrice fluctuates;
+    function setMinAmount(uint256 _minAmount) external onlyOwner() {
+        amountMinimum = _minAmount;
+        emit ChangedMinAmount(_minAmount);
+    }
+    
+    ///@dev set the token address(stablecoin) to convert to;
+    function setStableToConvert(address _tokenAddress) external onlyOwner() {
+        token = _tokenAddress;
+        emit ChangedAddressToken(_tokenAddress);
+    }
+    
+    ///@notice We implement a function that converts eth => usdt;
+    function convertEthToDai() external payable {
+        require(storedEth >= amountMinimum, "Not worth because of gas fee, we need more eth before transfer");
+        Iuniswap.sweepToken(token, amountMinimum, address(this));
+        //Iuniswap.pay();
+        
+        emit ConvertedEthToUsdt(msg.value);
+    }
+    
+    ///@dev helper to keep track of balances
+    function getBalanceOfETH() public view returns(uint256) {
+        return address(this).balance;
+    }
+    
+    function getBalanceOfUSDT() public view returns(uint256) {
+        return storedUsdt;
+    }
+    
+    //@notice allow to receive eth
+    receive() external payable{
+        
+    }
 }
