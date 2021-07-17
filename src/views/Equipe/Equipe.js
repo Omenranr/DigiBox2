52a9ea11190cabe87e3fc2f393fc9697a7677c80
kilian mongey <tumbleur.kilian@yahoo.fr>
2021-07-18 00:53:16 +0200

    page equipe et Prestataire

--- src/views/Equipe/Equipe.js
+++ src/views/Equipe/Equipe.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import "./Equipe.css";
 import Connection from '../../Connection';
+import NavBarDetail from "../../Components/NavBarDetail/NavBarDetail";
+import Alain from '../../Images/PhotoEquipe/Alain.png';
+import Nico from '../../Images/PhotoEquipe/Nico.png';
+import Teo from '../../Images/PhotoEquipe/Teo.png';
+import Kilian from '../../Images/PhotoEquipe/Kilian.png';
+import logo from "../../Images/TransparentLogo.png";
 
 export default function Equipe() {
     return (
+        <>
         <div className="Team">
             <h1 className="titleTeam"> L'Equipe derrière le projet.</h1> <hr></hr>
               <Connection />
         </div>
+
+         <div className="wholeTeam">
+         <img className="background" src={logo} alt="logo"/>
+             <div className="Teo">
+               <img  src={Teo} alt="teo"/>
+                 <h4>Teo Guilhermet</h4> 
+                    <p>Project manager et scrum master</p>
+             </div>
+
+                <div className="Alain" >
+                  <img src={Alain} alt="alain"/>
+                     <h4>Alain Praz</h4> 
+                        <p>Project manager et Product owner</p>
+                </div>
+
+                    <div className="Nico">
+                        <img  src={Nico} alt="nico"/>
+                            <h4>Nicolas Fruneau</h4>
+                               <p>Smart contract developer</p>
+                    </div>
+
+                      <div className="Kilian" >
+                       <img src={Kilian} alt="kilian"/>
+                            <h4>Kilian Mongey</h4> 
+                               <p>Smart contract developer</p>
+                      </div>
+         </div>
+         <NavBarDetail />
+         </>
     )
 }
--- src/views/Prestataire/Prestataire.js
+++ src/views/Prestataire/Prestataire.js
@@ -1,30 +1,106 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Prestataire.css";
 import Connection from '../../Connection';
+import Grid from '@material-ui/core/Grid';
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import Radio from "@material-ui/core/Radio";
+import NavBarDetail from "../../Components/NavBarDetail/NavBarDetail";
+import Container from '@material-ui/core/Container';
+import logo from "../../Images/TransparentLogo.png";
 
 export default function Prestataire() {
+    
+
     return (
         <div className="Prestataire-Prerequis">
             <h1 className="title"> Devenir partenaire.</h1> <hr></hr>
-             <Connection /> 
-            <div className="prerequis">
-            <h4> Voici les prérequis devenir un prestataire ches DigiBox:</h4>
-              <ul className="liste">
-                  <li>Etre inscrit au registre du commerce...</li>
-                  <li>Avoir un numéro de siret...</li>
-                  <li>Avoir un numéro de siret...</li>
-                  <li>Avoir un numéro de siret...</li>
-              </ul>
-            </div>
-            <div className="Form">
-                <h4>Afin de nous rejoindre, merci de remplir le formulair ci-dessous:</h4>
-                <form className="formulaire">
-                    <label for="email">Email address:</label>
-                    <input type="text" placeholder="email"></input> <br></br> <br></br>
-                    <label for="name">Nom de l'entreprise</label>
-                    <input type="text" placeholder="Nom"></input>
-                </form>
-            </div>
+             <Connection /> <br></br>
+
+             <h3 className="description"> Rejoignez l'équipe Digibox en quelques clicks !</h3>
+
+             <img className="logoPresta" src={logo} alt="logo"/>
+
+             <Container className="text" maxWidth="sm">
+                 <p> Avant de nous rejoindre veuillez lire attentivement nos CGV, notre whitePaper etc etc ... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+             </Container>
+
+             <Grid container alignItems="center" justify="center" direction="column">
+                <TextField
+                    id="nom-input"
+                    name="nom"
+                    label="Nom"
+                    type="text"
+                /> <hr></hr>
+             
+                <Grid item>
+                    <TextField
+                        id="age-input"
+                        name="age"
+                        label="Entreprise"
+                        type="text"
+                    />
+                </Grid> <hr></hr>
+
+                            <Grid item>
+                    <FormControl>
+                        <FormLabel>Secteur</FormLabel>
+                        <RadioGroup
+                        name="Hotellerie"
+                        >
+                        <FormControlLabel
+                            key="Hotellerie"
+                            value="0"
+                            control={<Radio size="small" />}
+                            label="Hotellerie"
+                        />
+                        <FormControlLabel
+                            key="Aventure"
+                            value="1"
+                            control={<Radio size="small" />}
+                            label="Aventure"
+                        />
+                        <FormControlLabel
+                            key="Gastronomie"
+                            value="2"
+                            control={<Radio size="small" />}
+                            label="Gastronomie"
+                        />
+                        <FormControlLabel
+                            key="Autres"
+                            value="3"
+                            control={<Radio size="small" />}
+                            label="Autres"
+                        />
+                        </RadioGroup>
+                    </FormControl>
+                    </Grid> <br></br>
+
+                    <Grid item>
+                    <TextField
+                        id="email-input"
+                        name="email"
+                        label="Email"
+                        type="text"
+                    />
+                </Grid> <hr></hr>
+
+                <Grid item>
+                    <TextField
+                        id="website"
+                        name="website"
+                        label="Link your website"
+                        type="text"
+                    />
+                </Grid> <hr></hr>
+
+             </Grid>
+             
+            <NavBarDetail /> 
+            
         </div>
     )
 } 
