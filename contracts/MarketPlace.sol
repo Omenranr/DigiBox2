fbe871353d23a286ee7fa2db1ff88d7a299c002b
kilian mongey <tumbleur.kilian@yahoo.fr>
2021-07-23 09:35:35 +0200

    withdrawBtn to improve

--- contracts/MarketPlace.sol
+++ contracts/MarketPlace.sol
@@ -3,49 +3,89 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract MarketPlace is Ownable, ERC20 {
+contract MarketPlace is Ownable {
+
+  using SafeMath for uint;
      
     uint256 productPrice;
     address recipient;
-    uint256 amountOfDai;
-    uint256 minAmountDai;
-    address swaperAddress;
-    address VaultAddress;
 
-    constructor() ERC20("", "") {}
+    uint256 public totalBalance;
+
+    //constructor() ERC20("", "") {}
 
     struct Payment {
         uint256 price;
         uint256 productId;
     }
 
+    struct Presta {
+      bool isAllowedToWithdraw;
+      uint256 amountToWithdraw;
+    }
+
     mapping(address => Payment[]) public payments;
+    mapping(address => Presta) public prestataire;
 
     event productBought(address From, uint256 Amount, uint256 productId);
     event priceChange(address From, uint256 Price);
+    event hasDeposited(address from, uint amountDeposited);
+    event isAllowedToPull(address isAllowed);
+    event hasWithdrawn(address from, uint amountWithdrawn);
 
-    function setPrice(uint256 _productPrice) public onlyOwner() {
-         productPrice = _productPrice;
-         emit priceChange(msg.sender, _productPrice);
+    function deposit() external payable {
+         totalBalance = totalBalance.add(msg.value);
     }
 
-    function buyProduct(uint256 productId) public payable {
-        require(msg.value >= productPrice, "You didn't send the correct amount");
+   function buyProduct(uint256 productId) public payable {
+      /*  require(msg.value >= productPrice, "You didn't send the correct amount");
         require(recipient != address(0), "ERC1155: transfer to 0 address");
 
          Payment memory _payment = Payment(productPrice, productId);
          payments[msg.sender].push(_payment);
 
-         emit productBought(msg.sender, msg.value, productId); 
+         //productPrice = productPrice.add(msg.value);
+
+         emit productBought(msg.sender, msg.value, productId); */
+    } 
+
+    function allowPresta(address _address) external onlyOwner() {
+        prestataire[_address].isAllowedToWithdraw = true;
+        emit isAllowedToPull(_address);
+    }
+
+    function withdrawFunds() public payable {
+      //uint256 amountToClaim = prestataire[msg.sender].amountToWithdraw;
+       
+      //require(prestataire[msg.sender].isAllowedToWithdraw == true);
+
+      //prestataire[msg.sender].amountToWithdraw = 0;
+
+      (bool success, ) = msg.sender.call{value: totalBalance}("");
+      require(success, "Failed to send to Presta");
+      
+      totalBalance = 0;
     }
 
     function getBalanceOfEth() public view returns(uint256) {
       return address(this).balance;
     }
 
+    receive() external payable{
+      
+    }
+}
+
 
   /*
+
+   function setPrice(uint256 _productPrice) public onlyOwner() {
+         productPrice = _productPrice;
+         emit priceChange(msg.sender, _productPrice);
+    }
+
     ///@notice we need to elaborate converter smart contract to deploy following
      function sendToSwaper() external onlyOwner() {
         require(address(this).balance >= 1 ether, "Not gas efficient to send now");
@@ -63,4 +103,4 @@ contract MarketPlace is Ownable, ERC20 {
         require(sent, "Failed to send Dai");
     } */
 
-}
+
--- src/views/Profil/Profil.js
+++ src/views/Profil/Profil.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Web3 from 'web3'
 import { makeStyles } from '@material-ui/core/styles';
 import Accordion from '@material-ui/core/Accordion';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
@@ -7,7 +8,10 @@ import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import NavBar from "../../Components/NavBar/NavBar";
 import NavBarDetail from "../../Components/NavBarDetail/NavBarDetail";
+import MarketPlaceContract from "../../contracts/MarketPlace.json";
 import "./Profil.css";
+//import getWeb3 from "../../getWeb3";
+//import { Button } from 'bootstrap';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -24,6 +28,7 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
+
 export default function Profil() {
     const classes = useStyles();
     const [expanded, setExpanded] = React.useState(false);
@@ -31,6 +36,52 @@ export default function Profil() {
     const handleChange = (panel) => (event, isExpanded) => {
         setExpanded(isExpanded ? panel : false);
     };
+
+    const [account, setAccount] = useState(null)
+    let [web3, setWeb3] = useState(null)
+    const [contract, setContract] = useState(null)
+  
+    useEffect(() => {
+      checkAccount()
+    }, [])
+
+    // invoke to check if account is already connected
+  async function checkAccount() {
+    let web3 = new Web3(window.ethereum)
+    setWeb3(web3)
+    const accounts = await web3.eth.getAccounts()
+    setAccount(accounts[0])
+
+    const networkId = await web3.eth.net.getId();
+    const deployedNetwork = MarketPlaceContract.networks[networkId];
+    const contract = new web3.eth.Contract(
+      MarketPlaceContract.abi,
+      deployedNetwork && deployedNetwork.address,
+    );
+
+    // const contractABI = require("../../../contracts/MarketPlace.json")
+    // const contract = new web3.eth.Contract(
+    //   contractABI,
+    //   '0xCd998a6949875C756DF54BDdE0A80Ac41F3B02d3'
+    // );
+
+    setContract(contract)
+    console.log('MarketPlace.js -> web3 loaded')
+  }
+
+  async function withdrawAll() {
+      console.log(account);
+      let amount = 1;
+
+      let gasValue = web3.utils.toHex(web3.utils.toWei('10', 'gwei'));
+      let totalAmount = web3.utils.toBN(amount);
+      var weiValue = web3.utils.toWei(totalAmount,'ether');
+      console.log(account, gasValue, weiValue);
+
+      await contract.methods.withdrawFunds().send({from: account, gas: gasValue, value: weiValue}, function(err, res) {})
+  }
+    
+
     return (
         <div className={classes.root}>
            <NavBar /> 
@@ -42,8 +93,8 @@ export default function Profil() {
                     aria-controls="panel1bh-contents"
                     id="panel1bh-header"
                     >
-                        <Typography className={classes.heading}> Your transactions </Typography>
-                        <Typography className={classes.secondaryHeading}> See transactions</Typography>
+                        <Typography className={classes.heading}> Your transactions. </Typography>
+                        <Typography className={classes.secondaryHeading}> See transactions.</Typography>
                     </AccordionSummary>
                     <ol>
                         <li>Input the transactions here</li>
@@ -56,8 +107,8 @@ export default function Profil() {
                     aria-controls="panel2bh-contents"
                     id="panel2bh-header"
                     >
-                        <Typography className={classes.heading}> See your NFT </Typography>
-                        <Typography className={classes.secondaryHeading}> See NFT</Typography>
+                        <Typography className={classes.heading}> See your NFT. </Typography>
+                        <Typography className={classes.secondaryHeading}> See NFT.</Typography>
                     </AccordionSummary>
                     <ol>
                         <li>Input the NFT here</li>
@@ -70,8 +121,8 @@ export default function Profil() {
                     aria-controls="panel3bh-contents"
                     id="panel3bh-header"
                     >
-                        <Typography className={classes.heading}> Your comments </Typography>
-                        <Typography className={classes.secondaryHeading}> See comments you've made</Typography>
+                        <Typography className={classes.heading}> Your comments. </Typography>
+                        <Typography className={classes.secondaryHeading}> See comments you've made.</Typography>
                     </AccordionSummary>
                     <ol>
                         <li>Input the comments received here</li>
@@ -84,16 +135,31 @@ export default function Profil() {
                     aria-controls="panel4bh-contents"
                     id="panel4bh-header"
                     >
-                        <Typography className={classes.heading}> Received comments </Typography>
-                        <Typography className={classes.secondaryHeading}> See received comments</Typography>
+                        <Typography className={classes.heading}> Received comments. </Typography>
+                        <Typography className={classes.secondaryHeading}> See received comments.</Typography>
                     </AccordionSummary>
                     <ol>
                         <li>Input the comments gave here</li>
                         <li>Input the comments gave here</li>
                     </ol>
                  </Accordion>
-             </div>
+                 <Accordion expanded={expanded === 'panel5'} onChange={handleChange('panel5')}>
+                    <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel4bh-contents"
+                    id="panel4bh-header"
+                    >
+                        <Typography className={classes.heading}> Your funds. </Typography>
+                        <Typography className={classes.secondaryHeading}> Withdraw available funds.</Typography>
+                    </AccordionSummary>
+                    <p>Display available funds : HERE</p>
+                    <button className="btn-with" onClick={() => {(withdrawAll())}}>Withdraw funds</ button><br></br>
+                 </Accordion><br></br>
+             </div><br></br>
            <NavBarDetail /> 
         </div>
     )
-}
+  }
+  
+  
+  ///onClick={() => {()}}
