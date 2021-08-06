f44ccc7af6ac63df145bb08f42888439ad8e817a
kilian mongey <tumbleur.kilian@yahoo.fr>
2021-08-06 23:30:21 +0200

    White, Road, Icon Git&Tele

--- src/views/RoadMap/RoadMap.js
+++ src/views/RoadMap/RoadMap.js
@@ -2,13 +2,14 @@ import React from 'react';
 import "./RoadMap.css";
 import NavBar from "../../Components/NavBar/NavBar";
 import NavBarDetail from "../../Components/NavBarDetail/NavBarDetail";
+import roadMap from "../../Images/RoadMap.png";
 
 export default function RoadMap() {
     return (
         <div className="RoadMap">
            <NavBar /> 
             <h1 className="road"> RoadMap. </h1> <hr></hr>
-
+            <img className="map" src={roadMap} alt="map"/>
            <NavBarDetail /> 
         </div>
     )
