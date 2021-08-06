da03c30ddcae0c5ca15d440bed456b642d85ca69
Nicofru <n.fruneau@gmail.com>
2021-08-07 01:53:48 +0200

    up

--- src/views/HomePage/HomePage.js
+++ src/views/HomePage/HomePage.js
@@ -1,36 +1,45 @@
-import React from "react";
+import React, { useState } from 'react';
 import NavBar from "../../Components/NavBar/NavBar";
 import NavBarDetail from "../../Components/NavBarDetail/NavBarDetail";
 import NFT from "../../Components/NFT/NFT";
 import "./HomePage.css";
 import logo from "../../Images/TransparentLogo.png";
 import Particles from 'react-particles-js';
+import Switch from '@material-ui/core/Switch';
 
 export default function HomePage() {
+    const [showParticles, setShowParticles] = useState(false);
+
+    const handleChange = (event) => {
+        setShowParticles(!showParticles);
+    };
+
     return (
         <div className="App">
             <NavBar />
+            <Switch checked={showParticles.checkedB} onChange={handleChange} color="primary" inputProps={{ 'aria-label': 'primary checkbox' }} />
+            {showParticles == true &&
             <Particles
-
                 params={{
                     particles: {
-                        retina_detect:true,
-                        move:{enable:true,speed:1,direction:"none",random:false,straight:false,out_mode:"out",bounce:false,attract:{enable:false,rotateX:600,rotateY:1200}},
+                        retina_detect: true,
+                        move: { enable:true,speed:1,direction:"none",random:false,straight:false,out_mode:"out",bounce:false,attract: { enable:false,rotateX:600,rotateY:1200 }},
                         number: { value: 80 },
                         density: { enable: true, value_area: 800 },
-                        color: { value: "#fff" },
-                        shape: { type: "circle", stroke: { width:0, color:"#000000"}, polygon:{ nb_sides:5 }},
-                        line_linked: { color: "#fff", distance: 150, enable: true, opacity: 0.4, width: 1 },
-                        opacity:{ value:0.5, random:false, anim:{enable:false,speed:1,opacity_min:0.1,sync:false}},
-                        size:{value:3,random:true,anim:{enable:false,speed:40,size_min:0.1,sync:false}}
+                        color: { value: "#4050b5" },
+                        shape: { type: "circle", stroke: { width:0, color:"#000000" }, polygon:{ nb_sides:5 }},
+                        line_linked: { color: "#4050b5", distance: 150, enable: true, opacity: 0.4, width: 1 },
+                        opacity: { value:0.5, random:false, anim:{ enable:false,speed:1,opacity_min:0.1,sync:false }},
+                        size: { value:3,random:true,anim: { enable:false,speed:40,size_min:0.1,sync:false }}
                     }
                 }}
             />
-                <h1 className="title">DigiBox, le plaisir d'offrir!</h1> <hr></hr>
-                    <div className="text-logo">
-                        <h2 className="subTitle">Les DigiBox du moment</h2>
-                    </div>
-                <NFT />
+            }
+            <h1 className="title">DigiBox, le plaisir d'offrir!</h1> <hr></hr>
+            <div className="text-logo">
+                <h2 className="subTitle">Les DigiBox du moment</h2>
+            </div>
+            <NFT />
             <NavBarDetail />
         </div>
     )
