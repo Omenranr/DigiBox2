ec1029d419ad862388746232b9a7defa9c629faa
Nicofru <n.fruneau@gmail.com>
2021-08-06 23:38:48 +0200

    up

--- src/App.js
+++ src/App.js
@@ -5,43 +5,11 @@ import Routes from './Routes'
 
 const App = () => {
   
-  // import React, { useState, useEffect } from "react";
-  // import MarketPlaceContract from "./contracts/MarketPlace.json";
-  // import getWeb3 from "./getWeb3";
-  // const [web3, setweb3] = useState(undefined)
-  // const [accounts, setAccounts] = useState(undefined)
-  // const [contract, setContract] = useState(undefined)
-
-  // useEffect(() => {
-  //   const init = async() => {
-  //     // Get network provider and web3 instance.
-  //     const web3 = await getWeb3();
-
-  //     // Use web3 to get the user's accounts.
-  //     const accounts = await web3.eth.getAccounts();
-
-  //     // Get the contract instance.
-  //     const networkId = await web3.eth.net.getId();
-  //     const deployedNetwork = MarketPlaceContract.networks[networkId];
-  //     const instance = new web3.eth.Contract(
-  //       MarketPlaceContract.abi,
-  //       deployedNetwork && deployedNetwork.address,
-  //     );
-
-  //     setweb3(web3)
-  //     setAccounts(accounts)
-  //     setContract(contract)
-  //   }
-  //   init()
-  // }, []) 
-
-    return (
-
+  return (
       <Router>
         <Routes />
       </Router>
     );
-
 }
 
 export default App;
--- src/views/HomePage/HomePage.js
+++ src/views/HomePage/HomePage.js
@@ -4,18 +4,34 @@ import NavBarDetail from "../../Components/NavBarDetail/NavBarDetail";
 import NFT from "../../Components/NFT/NFT";
 import "./HomePage.css";
 import logo from "../../Images/TransparentLogo.png";
-
+import Particles from 'react-particles-js';
 
 export default function HomePage() {
     return (
         <div className="App">
-        <NavBar />
-        <h1 className="title">DigiBox, le plaisir d'offrir!</h1> <hr></hr>
-                <div className="text-logo">
-                    <h2 className="subTitle">Les DigiBox du moment</h2>
-                </div>
-            <NFT />
-        <NavBarDetail />
+            <NavBar />
+            <Particles
+
+                params={{
+                    particles: {
+                        retina_detect:true,
+                        move:{enable:true,speed:1,direction:"none",random:false,straight:false,out_mode:"out",bounce:false,attract:{enable:false,rotateX:600,rotateY:1200}},
+                        number: { value: 80 },
+                        density: { enable: true, value_area: 800 },
+                        color: { value: "#fff" },
+                        shape: { type: "circle", stroke: { width:0, color:"#000000"}, polygon:{ nb_sides:5 }},
+                        line_linked: { color: "#fff", distance: 150, enable: true, opacity: 0.4, width: 1 },
+                        opacity:{ value:0.5, random:false, anim:{enable:false,speed:1,opacity_min:0.1,sync:false}},
+                        size:{value:3,random:true,anim:{enable:false,speed:40,size_min:0.1,sync:false}}
+                    }
+                }}
+            />
+                <h1 className="title">DigiBox, le plaisir d'offrir!</h1> <hr></hr>
+                    <div className="text-logo">
+                        <h2 className="subTitle">Les DigiBox du moment</h2>
+                    </div>
+                <NFT />
+            <NavBarDetail />
         </div>
     )
 }
