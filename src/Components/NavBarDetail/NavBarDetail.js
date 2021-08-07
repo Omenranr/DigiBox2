511f377e66dae5a5f8850a65dd2650e156d846f9
kilian mongey <tumbleur.kilian@yahoo.fr>
2021-08-07 17:52:26 +0200

    page création de offre

--- src/Components/NavBarDetail/NavBarDetail.js
+++ src/Components/NavBarDetail/NavBarDetail.js
@@ -2,17 +2,25 @@ import React from 'react';
 import "./NavBarDetail.css";
 import TelegramIcon from '@material-ui/icons/Telegram';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
 
 export default function NavBar() {
 
     return (
         <div>
             <p className="copyright">All rights reserved: Digibox fundation</p>
+            <a href="https://twitter.com/DigiBox10" target="_blank" raised >
+               {<TwitterIcon />}
+            </a>
             <a href="https://github.com/alpsoft/DigiBox" target="_blank" raised>
                 {<GitHubIcon />}
             </a>
             <a href="https://t.me/DigiBoxOfficialGroup" target="_blank" raised>
                 {<TelegramIcon/>}
+            </a>
+            <a href="https://www.instagram.com/digibox.blockchain/" target="_blank" raised>
+                {<InstagramIcon />}
             </a>
         </div>
     )
